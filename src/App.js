adc387682b64f4bdbc00dd7df236d555a778e9ab
Gisele <gisele_s@hotmail.com>
2023-01-25 19:54:59 -0300

    cadastro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div>OI</div>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./components/pages/login";
+import Cadastro from "./components/pages/cadastro";
+
+export default function App() {
+  
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/cadastro" element={<Cadastro />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+
